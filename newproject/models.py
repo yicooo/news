7552f97217c7b33fb6bf8b78484dbc86642f7843
yicooo <yico1997@hotmail.com>
2019-04-21 14:18:58 +0300

    unwanted comments are deleted

--- newproject/models.py
+++ newproject/models.py
@@ -5,11 +5,3 @@ class article(models.Model):
 
     title       = models.CharField(max_length= 20)
     source      = models.TextField()
-
-    # @classmethod
-    # def create(cls, title):
-    #     article = cls(title=title)
-        
-
-
-    #     return article
